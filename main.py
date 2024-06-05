8882c91bbb1a7f4226af8556c452accd24a79619
C4570 <julianfrancisco2010@gmail.com>
2024-06-05 11:52:17 -0300

    Busqueda directa, por genero y por autor, ademas de streamlit

--- main.py
+++ main.py
@@ -1,44 +1,135 @@
-# Libreries
-
-# !pip install streamlit
-# !pip install requests
-# !pip install beautifulsoup4
-
-import requests
-from bs4 import BeautifulSoup
 import streamlit as st
+import pandas as pd
+import Levenshtein
+import torch
+from transformers import BertTokenizer, BertModel
+from torch.nn.functional import cosine_similarity
 
-def scrape_lectulandia():
-    url = "https://ww3.lectulandia.com/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+file_path= 'Libros.parquet'
+libros = pd.read_parquet(file_path)
 
-    # Encuentra los elementos de la página web que contienen la información que quieres extraer.
-    # Por ejemplo, si los títulos de los libros están en etiquetas <h2>, podrías hacer:
-    book_titles = soup.find_all('h2')
 
-    # Luego puedes extraer el texto de estos elementos y agregarlo a tu dataset.
-    for title in book_titles:
-        print(title.text)
-        
-        
+# Cargar el tokenizador y el modelo BERT multilingüe
+tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
+model = BertModel.from_pretrained('bert-base-multilingual-cased')
+
+# Definir la función para obtener los embeddings de un texto
+def get_embedding(text):
+    # Tokenizar el texto, convirtiéndolo en tensores y asegurando la longitud máxima y el padding
+    tokens = tokenizer(text, return_tensors='pt', truncation=True, padding=True, max_length=512)
+    # Desactivar el cálculo del gradiente ya que estamos en modo de inferencia
+    with torch.no_grad():
+        # Pasar los tokens a través del modelo BERT para obtener los embeddings
+        outputs = model(**tokens)
+    # Promediar los embeddings de los tokens para obtener un solo vector de representación
+    return outputs.last_hidden_state.mean(dim=1)
+
+# Obtener los embeddings de las descripciones de los libros
+libros['Embedding'] = libros['Descripcion'].apply(get_embedding)
+
+# Configurar el título de la página
+st.set_page_config(page_title="Menú con Streamlit", layout="wide")
+
+#--------RECOMENDACIÓN DIRECTA----------#
+# Barra de búsqueda
+input_user = st.text_input('', value='¿Qué tienes ganas de leer hoy?')
+
+if input_user != '¿Qué tienes ganas de leer hoy?':
+    # Obtener el embedding de la entrada del usuario
+    user_embedding = get_embedding(input_user)
+    # Calcular la similitud del coseno entre el embedding del usuario y los embeddings de los libros
+    libros['Similitud'] = libros['Embedding'].apply(lambda x: cosine_similarity(user_embedding, x).item())
+    # Ordenar los libros por la similitud en orden descendente
+    libros_ordenados = libros.sort_values(by='Similitud', ascending=False)
     
-def main(book_titles):
-    st.title("Mi Aplicación de Lectura")
-    respuesta = st.text_input("¿Qué tienes ganas de leer hoy?")
-    if respuesta:
-        st.write(f"¡Genial! Buscaremos libros relacionados con {respuesta}.")
-        if st.checkbox('Show raw data'):
-            st.subheader('Raw data')
-            st.write(book_titles)
-        st.write("Aquí tienes algunas recomendaciones:")
+    # Retornar los primeros 'top_n' libros más similares
+    recomendaciones = libros_ordenados.head(3)
+    
+    # Mostrar las recomendaciones
+    for index, row in recomendaciones.iterrows():
+        print(f"Título: {row['Titulo']}")
+        print(f"Autor: {row['Autor']}")
+        print(f"Género: {', '.join(row['Genero'])}")
+        print(f"Descripción: {row['Descripcion']}\n")
+        print(f"Similitud: {row['Similitud']:.4f}\n")
+    
+    
+    
+    
+#--------GENERO----------#
+# Contar la cantidad de autores de cada autor
+gender_counts = libros['Genero'].explode().value_counts()
+
+# Barra de búsqueda
+input_user_g = st.text_input('', value='¿Qué genero tienes ganas de leer hoy?')
+# Lista de géneros únicos para la coincidencia difusa
+generos = set([g for sublist in libros['Genero'] for g in sublist])
+
+if input_user_g != '¿Qué genero tienes ganas de leer hoy?':
+    # Inicializar variables para almacenar el autor con la menor distancia y la distancia mínima
+    min_distance_g = float('inf')
+    closest_gender = None
+    
+    # Calcular la distancia de Levenshtein para cada autor
+    for genero in generos:
+        distance_g = Levenshtein.distance(genero, input_user_g)
+        if distance_g < min_distance_g:
+            min_distance_g = distance_g
+            closest_gender = genero
+    
+    # Filtrar el DataFrame para mostrar solo los libros del autor más cercano
+    filtered_df_g = libros[libros['Genero'].apply(lambda x: closest_gender in x)]
+    
+    # Crear columnas para mostrar lado a lado
+    col3, col4 = st.columns(2)
+    
+    # Mostrar el DataFrame filtrado en la primera columna
+    with col3:
+        st.write(f"Libros recomendados del genero {closest_gender}:")
+        st.dataframe(filtered_df_g)
+    
+    # Mostrar la cantidad de autores de cada autor en la segunda columna
+    with col4:
+        st.write("Cantidad de libros por genero:")
+        st.dataframe(gender_counts)
         
         
+        
+        
+        
+#--------AUTOR----------#
+# Contar la cantidad de autores de cada autor
+author_counts = libros['Autor'].explode().value_counts()
 
+# Barra de búsqueda
+input_user_a = st.text_input('', value='¿Qué autor tienes ganas de leer hoy?')
+# Lista de géneros únicos para la coincidencia difusa
+autores = set([g for sublist in libros['Autor'] for g in sublist])
 
-if __name__ == "__main__":
-    scrape_lectulandia()
-    main(book_titles)
-
-
-
+if input_user_a != '¿Qué autor tienes ganas de leer hoy?':
+    # Inicializar variables para almacenar el autor con la menor distancia y la distancia mínima
+    min_distance_a = float('inf')
+    closest_author = None
+    
+    # Calcular la distancia de Levenshtein para cada autor
+    for autor in autores:
+        distance_a = Levenshtein.distance(autor, input_user_a)
+        if distance_a < min_distance_a:
+            min_distance_a = distance_a
+            closest_author = autor
+    
+    # Filtrar el DataFrame para mostrar solo los libros del autor más cercano
+    filtered_df_a = libros[libros['Autor'].apply(lambda x: closest_author in x)]
+    
+    # Crear columnas para mostrar lado a lado
+    col1, col2 = st.columns(2)
+    
+    # Mostrar el DataFrame filtrado en la primera columna
+    with col1:
+        st.write(f"Libros recomendados del autor {closest_author}:")
+        st.dataframe(filtered_df_a)
+    
+    # Mostrar la cantidad de autores de cada autor en la segunda columna
+    with col2:
+        st.write("Cantidad de libros por autor:")
+        st.dataframe(author_counts)
